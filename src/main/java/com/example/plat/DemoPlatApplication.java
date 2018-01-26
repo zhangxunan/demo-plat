330c05041e002011ec36d1d21c2cbf4009d2e63e
章旭楠 <zhang_xn1314@163.com>
2018-01-26 11:41:10 +0800

    Initial commit

---
+++ src/main/java/com/example/plat/DemoPlatApplication.java
@@ -0,0 +1,12 @@
+package com.example.plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPlatApplication.class, args);
+	}
+}
